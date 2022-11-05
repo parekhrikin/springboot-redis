959fd7a26720105d2520e0407a91d50c6440ac4d
Rikin Parekh <parekh.r@northeastern.edu>
2022-11-05 02:58:42 -0400

    patch completed!

--- src/main/java/neu/edu/info7255/springboot/controller/PlanController.java
+++ src/main/java/neu/edu/info7255/springboot/controller/PlanController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 //import neu.edu.info7255.springboot.entity.Plan;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
 import neu.edu.info7255.springboot.repository.PlanDao;
@@ -16,9 +17,7 @@ import org.everit.json.schema.loader.SchemaLoader;
 
 import javax.json.JsonMergePatch;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static java.util.Currency.getInstance;
 
@@ -29,7 +28,7 @@ public class PlanController {
 
     @Autowired
     private PlanDao dao;
-    ObjectMapper mapper = new ObjectMapper();
+    static ObjectMapper mapper = new ObjectMapper();
 
     @PostMapping("/schema")
     public JsonNode saveSchema(@RequestBody JsonNode schema){
@@ -67,43 +66,76 @@ public class PlanController {
 //        return new String(Files.readAllBytes(Paths.get(file)));
 //    }
 
-    @PatchMapping(value = "/{id}/{fNames}")
-    public JsonNode updatePlan(@PathVariable("id") String id, @PathVariable("fNames") String fieldNames, @RequestBody JsonNode patch) throws IOException {
-        String[] fields = fieldNames.split("\\+");
-        LinkedList<String> ll = new LinkedList<String>();
-        for(int i = 0; i < fields.length; i++){
-            ll.add(fields[i]);
+    @PatchMapping(value = "/{id}/{objType}")
+    public List<JsonNode> updatePlan(@PathVariable("id") String id, @PathVariable("objType") String objectType, @RequestBody JsonNode patch) throws IOException {
+//        String[] fields = objectType.split("\\+");
+//        LinkedList<String> ll = new LinkedList<String>();
+//        for(int i = 0; i < fields.length; i++){
+//            ll.add(fields[i]);
+//        }
+
+        List<JsonNode> plansPatched = new ArrayList<>();
+
+        if(objectType.equals("plan")){
+            JsonNode plan = dao.findPlanById(id);
+            plansPatched.add(applyPatchToPlan(patch, plan, objectType, id));
+            dao.update(plansPatched.get(0), id);
+        } else if(objectType.equals("planservice")){
+            int i = 0;
+            List<JsonNode> plans = dao.findAll();
+            for(JsonNode plan: plans){
+                plansPatched.add(applyPatchToPlan(patch, plan, objectType, id));
+                dao.update(plansPatched.get(i), plansPatched.get(i).get("objectId").toString());
+                i++;
+            }
+        } else {
+            int i = 0;
+            List<JsonNode> plans = dao.findAll();
+            for(JsonNode plan: plans){
+                plansPatched.add(applyPatchToPlan(patch, plan, objectType, id));
+                dao.update(plansPatched.get(i), plansPatched.get(i).get("objectId").toString());
+                i++;
+            }
         }
-        JsonNode plan = dao.findPlanById(id);
-        JsonNode planPatched = applyPatchToPlan(patch, plan, ll);
-        dao.update(planPatched, id);
-        return planPatched;
+
+
+
+        return plansPatched;
     }
 
-    public static JsonNode applyPatchToPlan(JsonNode patch, JsonNode plan, LinkedList<String> ll) {
-        JsonNode patched = traverse(patch, plan, ll);
+    public static JsonNode applyPatchToPlan(JsonNode patch, JsonNode plan, String objectType, String id) {
+        JsonNode patched = traverse(patch, plan, objectType, id);
         return patched;
     }
 
-    public static JsonNode traverse(JsonNode patch, JsonNode root, LinkedList<String> ll) {
+    public static JsonNode traverse(JsonNode patch, JsonNode root, String objectType, String id) {
 
         if(root.isObject()){
             Iterator<String> fieldNames = root.fieldNames();
 
             while(fieldNames.hasNext()) {
                 String fieldName = fieldNames.next();
-                if(fieldName.equals(ll.getFirst())){
-
-                    ll.removeFirst();
-                }
                 JsonNode fieldValue = root.get(fieldName);
-                traverse(patch, fieldValue, ll);
+                if(fieldValue.isObject() && fieldValue.get("objectType").asText().equals(objectType) && fieldValue.get("objectId").asText().equals(id)){
+                    Iterator<Map.Entry<String, JsonNode>> fields = patch.fields();
+                    while(fields.hasNext()){
+                        Map.Entry<String, JsonNode> field = fields.next();
+                        ((ObjectNode) root.get(fieldName)).put(field.getKey(), field.getValue());
+                    }
+                } else if(fieldValue.isArray()){
+//                    Iterator<Map.Entry<String, JsonNode>> objs = patch.fields();
+                    for(JsonNode obj: patch){
+//                        Map.Entry<String, JsonNode> obj = objs.next();
+                        ((ArrayNode) root.get(fieldName)).add(obj);
+                    }
+                }
+                traverse(patch, fieldValue, objectType, id);
             }
         } else if(root.isArray()){
             ArrayNode arrayNode = (ArrayNode) root;
             for(int i = 0; i < arrayNode.size(); i++) {
                 JsonNode arrayElement = arrayNode.get(i);
-                traverse(patch, arrayElement, ll);
+                traverse(patch, arrayElement, objectType, id);
             }
         } else {
             // JsonNode root represents a single value field - do something with it.
